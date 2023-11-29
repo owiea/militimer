b7bf63b1a8e285afb624b61e2a35665f8107007f
siddhantbapna <siddhantbapnaofficial@gmail.com>
2023-11-29 19:52:42 +0530

    1.00.10

--- control.js
+++ control.js
@@ -46,10 +46,10 @@ function startTimer() {
         const v = new Date(curr);
         z = 500
         document.querySelector("#main1").innerText = z
-        document.querySelector("#timer").innerText = v.getHours() + ":" + v.getMinutes() + ":" + v.getSeconds() + ":" + v.getMilliseconds()
+        document.querySelector("#timer").innerText = v.getHours() + ":" + v.getMinutes() + ":" + v.getSeconds() + ":" + v.getMilliseconds()/10
         if (curr - 946665000000 === 30) {
             navigator.vibrate(z)
-            alert()
+            //alert()
         }
     }, 10);
 }
